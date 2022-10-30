45965c6322457e0ec98b52598e4c7d8310a10de2
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-10-30 23:09:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  const x ;
+  if (x=0){
+    console.log('logged')
+  }
+    else{
+      return ( 'x is zero')
+    }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
